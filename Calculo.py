627a5691aae1336a8df872e8dbd7656f34ff5db0
zapluc75 <lucianopaz@yahoo.com.br>
2025-08-14 21:01:45 -0300

    Update Calculo.py

--- Calculo.py
+++ Calculo.py
@@ -101,7 +101,7 @@ if st.session_state.validado:
             st.session_state.resultado = dados_exportar
             st.session_state["dados_tipo"] = {
                 "TipoCam": tipo,
-                "TamMaxPermitido": f"{linha['Tamax']:.1f}",
+                "TamMaxPermit": f"{linha['Tamax']:.1f}",
                 "AET": linha["AET"],
                 "OBS": linha["OBS"],
             }
