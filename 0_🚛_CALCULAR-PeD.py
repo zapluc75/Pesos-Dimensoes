31656e9137be74044310730f73969422af9dea9c
zapluc75 <lucianopaz@yahoo.com.br>
2026-02-02 14:13:29 -0300

    Update 0_🚛_CALCULAR-PeD.py

--- 0_🚛_CALCULAR-PeD.py
+++ 0_🚛_CALCULAR-PeD.py
@@ -15,7 +15,7 @@ from utils import (
 )
 
 st.set_page_config(page_title="Apuração Pesos e Dimensões - DERDF", page_icon="📦", layout="centered")
-st.write(st.secrets)
+st.write(st.secrets.keys())
 
 if "login_realizado" not in st.session_state: # Gate de login (mantido do projeto original)
     verificar_login()
