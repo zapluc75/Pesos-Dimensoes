00f0e914f16e5914062846fc3d3077bcccf0d695
zapluc75 <lucianopaz@yahoo.com.br>
2026-05-21 16:00:37 -0300

    Update 0_🚛_CALCULAR-PeD.py

--- 0_🚛_CALCULAR-PeD.py
+++ 0_🚛_CALCULAR-PeD.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 from st_copy import copy_button
 import textwrap
+import streamlit.components.v1 as components
 
 from utils import (
     verificar_login,
@@ -36,7 +37,40 @@ st.title("📦 Apuração Pesos e Dimensões")
 # Seletor do tipo + entradas principais
 col1, col2, col3 = st.columns(3)
 with col1:
-    tipo = st.selectbox("Selecione o Tipo de Caminhão", tabela["Codigo"].unique(), key="tipo")
+    st.markdown("""
+<style>
+div[data-baseweb="select"] input {
+    caret-color: transparent;
+}
+</style>
+""", unsafe_allow_html=True)
+
+# JavaScript para bloquear teclado virtual
+components.html("""
+<script>
+function bloquearTeclado() {
+    const selects = window.parent.document.querySelectorAll(
+        'div[data-baseweb="select"] input'
+    );
+
+    selects.forEach((input) => {
+        input.setAttribute("readonly", true);
+        input.setAttribute("inputmode", "none");
+    });
+}
+
+setTimeout(bloquearTeclado, 500);
+</script>
+""", height=0)
+
+# Selectbox
+tipo = st.selectbox(
+    "Selecione o Tipo de Caminhão",
+    tabela["Codigo"].unique(),
+    key="tipo"
+)
+
+st.write(tipo)
 with col2:
     comprimento = st.number_input("Comprimento (em metros)", min_value=0.0, key="comprimento")
 with col3:
