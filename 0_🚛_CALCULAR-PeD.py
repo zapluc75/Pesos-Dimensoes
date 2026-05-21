564e1bdc8611d04e5d26e338721d00e82944985b
zapluc75 <lucianopaz@yahoo.com.br>
2026-05-21 14:07:10 -0300

    Update 0_🚛_CALCULAR-PeD.py

--- 0_🚛_CALCULAR-PeD.py
+++ 0_🚛_CALCULAR-PeD.py
@@ -48,7 +48,7 @@ qt_tara = int(linha["Qtara"])
 nome_caminhao = linha["Nome"]
 eixos = linha["Eixos"]
 desenho = linha["Desenho"]
-st.info(f" **{nome_caminhao}** / **{eixos}** / **{desenho}**")
+st.info(f" **{nome_caminhao}** / **{eixos}** eixos / **{desenho}**")
 
 # Entradas por unidade (placa + tara)
 placas = []
