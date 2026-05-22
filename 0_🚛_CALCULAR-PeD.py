724bdadd424be9610afb957048a1795f238b43ba
zapluc75 <lucianopaz@yahoo.com.br>
2026-05-22 14:21:45 -0300

    Update 0_🚛_CALCULAR-PeD.py

--- 0_🚛_CALCULAR-PeD.py
+++ 0_🚛_CALCULAR-PeD.py
@@ -145,7 +145,7 @@ if st.session_state.calculado:
         Classificação por eixos:  {tipo}  
         De acordo com a Portaria 268/2022 do Senatran  
         Veículo/Combinação: {r["Placas"]}  
-        Comprimento Total: ( ) metros  
+        Comprimento Total: (xxx) metros  
         Tara: {r["TaraTotal"]:.0f} kg - (conforme plaquetas)  
         Carga: {r["PesoLiqNF"]:.0f} kg - (produto)  NF nº xxxxxxx  
         PBTC apurado: {r["PBT-PBTC"]:.0f} kg  
